985d030be2ddfb2de6365fa64f44a0b1bfff497d
53rNa <53rNa76@gmail.com>
2024-07-17 23:00:52 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as dF
+from true_math import divide as dT
+
+result1 = dF(69, 3)
+result2 = dF(3, 0)
+result3 = dT(49, 7)
+result4 = dT(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
